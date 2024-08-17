e8518d3f45a1ab1e188df74ddd1b74e43839091a
unknown <prachipanwar397@gmail.com>
2024-08-17 11:07:36 +0530

    second problem to increment a number

--- JS-101/index2.js
+++ JS-101/index2.js
@@ -3,6 +3,10 @@
 // Return the number after Incrementing it by 1
 
 function incrementNumber(a){
+    return ++a; // here we can't use a++ as it will return a then increment its value 
+}
+// or we can use this method also
+function incrementNumber(a){
     let incre = a+1;
     return incre;
 }
