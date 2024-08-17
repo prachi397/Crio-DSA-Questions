0f5da6d0c0ab95feca5655fedcf09a21e9c4b3af
unknown <prachipanwar397@gmail.com>
2024-08-17 17:43:07 +0530

    kth character from starting and end of the string

--- JS-101/index14.js
+++ JS-101/index14.js
@@ -11,3 +11,19 @@ if (kthCharFromStart("abcdefghi", 2) !== 'b')
     console.log("Test fails: Expected 'b' for input str = 'abcdefghi' and k = 2");
 else
     console.log("Sample test case for input str = 'abcdefghi' and k = 2 passed!");
+
+// Problem Description
+// Implement the function kthCharFromEnd() which Accepts 2 arguments - a string str and a number k
+// Returns the kth character of the string str from the end
+function kthCharFromEnd(str, k) {
+    let len = str.length;
+    return str[len - k];
+}
+console.log(kthCharFromEnd('prachi', 3));
+
+//sample test case
+
+if (kthCharFromEnd("abcdefghi", 2) !== 'h')
+    console.log("Test fails: Expected 'h' for input str = 'abcdefghi' and k = 2");
+else
+    console.log("Sample test case for input str = 'abcdefghi' and k = 2 passed!");
