eade82ce5fd4341dd5110019e0cd21e792c48733
unknown <prachipanwar397@gmail.com>
2024-08-18 23:14:51 +0530

    update property in object

---
+++ JS-101/objects/index3.js
@@ -0,0 +1,16 @@
+// Problem Description
+// Implement the function updateProperty() that
+// Accepts three arguments an object obj, a string representing a property name propName, and a string 
+// str, Updates the property propName of the input object obj with the value str
+// Returns the updated property
+
+function updateProperty(obj, propName, str) {
+    return propName = str;
+}
+console.log(updateProperty({ firstName: 'Alex' }, 'firstName', 'John'));
+
+//sample test case
+if ((updateProperty({ firstName: 'Alex' }, 'firstName', 'John')) !== 'John')
+    console.log("Test fails: Expected 'John' for input  obj = { firstName: 'Alex' }, propName = 'firstName', str = 'John'");
+else
+    console.log("Sample test case for input obj = { firstName: 'Alex' }, propName = 'firstName', str = 'John' passed!");
