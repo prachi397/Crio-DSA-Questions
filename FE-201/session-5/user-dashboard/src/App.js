140f384801b44cd7f6bc98de9dc6d663ae2f2408
unknown <prachipanwar397@gmail.com>
2024-10-04 16:10:11 +0530

    changes

--- FE-201/session-5/user-dashboard/src/App.js
+++ FE-201/session-5/user-dashboard/src/App.js
@@ -2,10 +2,14 @@ import DogPics from './component/DogPics';
 import Home from './component/Home';
 // import './App.css';
 import BookAppointment from './component/BookAppointment';
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext();
 
 function App() {
   return (
     <div className="App">
+    {/* <UserInfo/> */}
      {/* <Home/> */}
      {/* <DogPics/> */}
      <BookAppointment/>
