ac4fbbf732703a86f2b3e59122b33b869a6bfdc3
unknown <prachipanwar397@gmail.com>
2024-10-17 09:47:30 +0530

    find natural sum with formula

--- DSA-101/naturalSum.js
+++ DSA-101/naturalSum.js
@@ -2,10 +2,14 @@
 // You are given a positive integer 'N'. You have to find the sum of the first 'N' natural numbers.
 
 function findNaturalSum(N){
-    let sum=0;
-    for(let i=1;i<=N;i++){
-        sum += i;
-    }
-    return sum;
+    //first approach -->
+    // let sum=0;
+    // for(let i=1;i<=N;i++){
+    //     sum += i;
+    // }
+    // return sum;
+
+    //second approach -->
+    return N*(N+1)/2;
 }
 console.log(findNaturalSum(5))
