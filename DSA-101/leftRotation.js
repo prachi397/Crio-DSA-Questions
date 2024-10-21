7db997a3dd7513257a695c53e9e6216b71a4db26
unknown <prachipanwar397@gmail.com>
2024-10-21 20:28:22 +0530

    left rotation of array elements

---
+++ DSA-101/leftRotation.js
@@ -0,0 +1,20 @@
+// Problem Description
+// You are given a array of numbers. Your task is to rotate the given array left(anti-clockwise) 
+// by 1 units from the starting index. You are required to return the rotated array.
+// Sample Input 1
+// 1 2 3 4 5
+// Sample Output 1
+// 2 3 4 5 1
+
+function leftRotate(arr){
+    let rotatedArray = [];
+    let n = arr.length;
+    let temp = arr[0];
+    for(let i=0;i<n-1;i++){
+        arr[i] = arr[i+1];
+    }
+    arr[n-1] = temp;
+    return arr;
+}
+
+console.log(leftRotate([1,2,3,4,5]));
