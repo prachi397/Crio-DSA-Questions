4a8750fc345eb0113b022fac8256b76dcda86c25
unknown <prachipanwar397@gmail.com>
2024-10-21 19:43:52 +0530

    sort array in lexicographical order

---
+++ DSA-101/sortArray.js
@@ -0,0 +1,8 @@
+// Problem Description
+// Given an array of n strings. Sort the array in lexicographical order.
+
+function sortArray(arr){
+    return arr.sort();
+}
+
+console.log(sortArray(["abc","abd","ab","a"]));
