a0cb93943db10def3538334914f302741f07437d
unknown <prachipanwar397@gmail.com>
2024-10-23 19:46:00 +0530

    capitalise first character of each word of a string without using inbuilt method

--- DSA-101/stringCapitalise.js
+++ DSA-101/stringCapitalise.js
@@ -11,7 +11,16 @@
 
 function capitalizedString(para){
     //first approach
-    let word = para.split(' ');
-    return word.map((word)=>word.charAt(0).toUpperCase()+word.slice(1)).join(' ');
+    // let word = para.split(' ');
+    // return word.map((word)=>word.charAt(0).toUpperCase()+word.slice(1)).join(' ');
+
+    //second approach without using inbuilt methods
+    let word = Array.from(para);
+    for(let i=0;i<word.length;i++){
+        if(i==0 || word[i-1] == ' '){
+            word[i] = word[i].toUpperCase();
+        }
+    }
+    return word.join('');
 }
 console.log(capitalizedString("the quick Brown fox jumps over The lazy dog."));
