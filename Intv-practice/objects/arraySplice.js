8e769a9d2455a113af4b32f5089602b0be586dc1
unknown <prachipanwar397@gmail.com>
2024-12-10 14:43:12 +0530

    array splice method

--- Intv-practice/objects/arraySplice.js
+++ Intv-practice/objects/arraySplice.js
@@ -2,7 +2,7 @@
 //the first argument specifies the array index for insertion/deletion, and the optinal second argument
 //specifies the number of elements to be deleted and the rest of the arguments will be elements to inserted
 //in the array.
-// Note: it will not modify the original array.
+// Note: splice method modify the original array and returns deleted array.
 let arr1 = [1,2,3,4,5,6];
 console.log(arr1.splice(0,2));
 let arr2 = [1,2,3,4,5,6];
