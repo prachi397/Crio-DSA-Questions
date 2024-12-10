657b09c7a96d11ed518fad8bbcf5c2fa9ea9e2f3
unknown <prachipanwar397@gmail.com>
2024-12-10 17:27:06 +0530

    first class functions in javacscript

--- Intv-practice/functions/arrowFunc.js
+++ Intv-practice/functions/arrowFunc.js
@@ -1,5 +1,16 @@
-//An arrow function or lambda expression is a shorter and concise way of creating functions in javascript.
-//arrow functions don't have its own this, argument and super. They can not be used as constructor.
+//Arrow Function -> An arrow function or lambda expression is a shorter and concise way of creating 
+//                  functions in javascript.arrow functions don't have its own this, argument and super. 
+//                  They can not be used as constructor.
 
 const arrowFunc = (a,b)=>a+b;
 console.log(arrowFunc(5,4));
+
+//First class function -> first class functions in javascript are first class objects. They are treated like
+//                        any other variables.They can be passed as an argument of other function, can be
+//                        returned by a function and can be assigned to a variable.
+
+// Assigning a function to a variable
+const greet = function(name) {
+    return `Hello, ${name}!`;
+  };
+console.log(greet("Prachi"));
