fe5e9925e24367539a7e66237def02fc35164b22
unknown <prachipanwar397@gmail.com>
2024-12-10 17:03:28 +0530

    arrow functions in javacsript

---
+++ Intv-practice/functions/arrowFunc.js
@@ -0,0 +1,5 @@
+//An arrow function or lambda expression is a shorter and concise way of creating functions in javascript.
+//arrow functions don't have its own this, argument and super. They can not be used as constructor.
+
+const arrowFunc = (a,b)=>a+b;
+console.log(arrowFunc(5,4));
