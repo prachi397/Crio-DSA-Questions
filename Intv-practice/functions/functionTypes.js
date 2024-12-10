03114b5d01308e96b3dba6872bbc309a55711a4a
unknown <prachipanwar397@gmail.com>
2024-12-10 19:11:03 +0530

    function curring example

--- Intv-practice/functions/functionTypes.js
+++ Intv-practice/functions/functionTypes.js
@@ -1,11 +1,11 @@
-//Arrow Function -> An arrow function or lambda expression is a shorter and concise way of creating 
+//1. Arrow Function -> An arrow function or lambda expression is a shorter and concise way of creating 
 //                  functions in javascript.arrow functions don't have its own this, argument and super. 
 //                  They can not be used as constructor.
 
 const arrowFunc = (a,b)=>a+b;
 console.log(arrowFunc(5,4));
 
-//First class function -> first class functions in javascript are first class objects. They are treated like
+//2. First class function -> first class functions in javascript are first class objects. They are treated like
 //                        any other variables.They can be passed as an argument of other function, can be
 //                        returned by a function and can be assigned to a variable.
 
@@ -15,13 +15,13 @@ const greet = function(name) {
   };
 console.log(greet("Prachi"));
 
-//First order function -> first order function is a function that does not take another function as an argumnet
+//3. First order function -> first order function is a function that does not take another function as an argumnet
 //                        and does not return a function as its return value
 
 const firstOrderFunc = () => console.log("First order function");
 firstOrderFunc();
 
-//Higher order function -> higher order function is a function that accepts a function as an argumnet or 
+//4. Higher order function -> higher order function is a function that accepts a function as an argumnet or 
 //                         return a function as its return value.
 function greetings(){
     return "hello, good morning.";
@@ -31,6 +31,24 @@ function higherOrderFunc(greetings){
 }
 console.log(higherOrderFunc(greetings()));
 
-//unary function -> function that accepts exactly one argument
+//5. unary function -> function that accepts exactly one argument
 const unaryFunc = (a) => console.log(a+10);
 unaryFunc(5);
+
+//6. currying functiom -> currying is the process of taking a function with multiple argument and turning 
+//                        it into sequence of functions each having only a single argumnet. By using currying
+//                        we convert n-ary function into unary function.
+
+const curryingFunc = (a)=>(b)=>(c)=>a+b+c;
+curryingFunc(1); //1+b+c
+curryingFunc(1)(2); //1+2+c
+console.log(curryingFunc(1)(2)(3));
+//another example of function curring
+function multiple(a){
+  return function(b){
+    return a*b;
+  };
+}
+const multiplyOfTwo = multiple(2);
+const multiplyOfFive = multiplyOfTwo(5);
+console.log(multiplyOfFive);
