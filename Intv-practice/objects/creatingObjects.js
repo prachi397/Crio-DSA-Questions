fcc565726f6f9ce20a57da45de0fdf69ef040208
unknown <prachipanwar397@gmail.com>
2024-12-10 10:23:27 +0530

    create object using Object.create method

--- Intv-practice/objects/creatingObjects.js
+++ Intv-practice/objects/creatingObjects.js
@@ -1,7 +1,32 @@
 //different ways to create an object
+
 //1. Object literal syntax --> comma separate set of name-value pair
 let obj1 = {
-    name:"Prachi",
-    age:22
-}
+  name: "Prachi",
+  age: 22,
+};
 console.log(obj1);
+
+//2. using Object constructor
+let obj2 = Object(); //this will create empty object
+//add values to the object
+obj2.vehical = "car";
+obj2.color = "black";
+console.log(obj2);
+
+//3. create method -> Object.create method is used to create a object pass passing the specified prototype
+//                    object and properties as arguments, this is helpful by creating new objects based on
+//                    existing objects. the second argument is optinal.
+
+let obj3 = Object.create(null); //creates a new object which has protype null
+console.log(obj3);
+//example of creating object using object.create
+let vehical = { wheels: "4", fuelType: "Gasoline", color: "green" };
+let carProps = {
+  type: { value: "volkswagen", enumerable: true },
+  model: { value: "golf", enumerable: true },
+};
+var car = Object.create(vehical, carProps);
+console.log(Object.getPrototypeOf(car));
+console.log(car);
+console.log(car.wheels);
