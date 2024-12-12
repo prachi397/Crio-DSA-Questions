1ed5b25e695f8c52ec401a595055b799479172bb
unknown <prachipanwar397@gmail.com>
2024-12-12 21:47:59 +0530

    hoisitng

--- Intv-practice/variables/letVarConst.js
+++ Intv-practice/variables/letVarConst.js
@@ -54,6 +54,8 @@ switch (counter){
         break;
     }
 }
+//Hoisting -> Hoisting is a mechanism where variable and function declaration moves to the top of their
+//            containing scope.Javascript only hoist declarations not initialisation.
 
 //Temporal Dead Zone -> Temporal dead zone is a specific period or area of a block where the is inaccessible
 //                      untill it is initialized with a value. This behaviour occurs when a variable is
@@ -66,13 +68,3 @@ function TDZ(){
     var counter2 = 10;
 }
 TDZ();
-
-//IIFE (Immediately Invoked Function Expression) -> IIFE is a javascript function that runs as soon as it is 
-//                                                  defined. The primary reason to use an IIFE is to obtain 
-//                                                  data privacy because any variables declared within the IIFE 
-//                                                  cannot be accessed by the outside world. 
-
-(function(){
-    const msg = "Hello World!";
-    console.log(msg);
-})();
