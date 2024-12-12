e290b0552b2a57ef9391aa3ff3ba83acbd5897c4
unknown <prachipanwar397@gmail.com>
2024-12-12 20:25:06 +0530

    immediately invoked function expression

--- Intv-practice/variables/letVarConst.js
+++ Intv-practice/variables/letVarConst.js
@@ -66,3 +66,13 @@ function TDZ(){
     var counter2 = 10;
 }
 TDZ();
+
+//IIFE (Immediately Invoked Function Expression) -> IIFE is a javascript function that runs as soon as it is 
+//                                                  defined. The primary reason to use an IIFE is to obtain 
+//                                                  data privacy because any variables declared within the IIFE 
+//                                                  cannot be accessed by the outside world. 
+
+(function(){
+    const msg = "Hello World!";
+    console.log(msg);
+})();
