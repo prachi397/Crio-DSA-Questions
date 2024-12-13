6decdcb920e3cacfca1adeddce5a181a59e2973d
unknown <prachipanwar397@gmail.com>
2024-12-13 10:30:26 +0530

    local and session storage

--- Intv-practice/variables/letVarConst.js
+++ Intv-practice/variables/letVarConst.js
@@ -72,3 +72,45 @@ TDZ();
 //scope -> scope in javascript is the accessibility of variables, functions, objects in some particular
 //         part of our code during runtime. In other words we can say that scope determines the 
 //         visibilities of variables. functions and objects in a particular part/area of our code.
+
+//Service Worker -> A service worker is basically a script (javascript file), which runs in the background
+//                  seperate from the web page and provide features that do not required any user interaction.
+//                  Some of the major features of service worker are Rich offline experiecnes, periodicly
+//                  background syncs, push notifications, handle network requests and manage a cache of 
+//                  responses.
+// Manipulate DOM using Service Worker -> It can not directly manipulate DOM but it can communicate with
+//                                        the pages it controls, by responding to message sent via PostMessage
+//                                        interface and those pages can manipulate the DOM.
+// How do you reuse information across service worker restarts?
+// The problem with service worker is that it gets terminated when not in user and restart again when it
+// requires, so we can not rely on global state within a service worker's onfecth and onmessage handlers.
+// In this case, service worker will have access to indexedDB API in order to persist and reuse information
+// across restarts.
+
+// IndexedDB API -> IndexedDB is a low-level api for client-sdie storage of larger amount of structured data,
+//                  including files/blobs. This API uses indexes to enable high performance searches
+//                  of this data.
+
+//Web Storage -> Web storage is an api that provides a mechanism by which browser can store data as a 
+//               key-value pair locally within the browser in a more in concise manner than using cookies. 
+//               web storage provides two mechanism to store data on client side :
+//1. Local Stroage -> Local Storage allows us to store data in the browser with no-expireation time. The
+//                    data is stored in local storage remains available even the browser is closed or 
+//                    reopened, or when the user navigates to another pages. The data is domain specific that
+//                    means data is accessible only on the same domain and Maximum storage capacity is 
+//                    approximately 5–10 MB (varies by browser).
+
+// //store the data in local storage
+// localStorage.setItem({"name":"PrachiPanwar"});
+// //get the data from local storage
+// console.log("local storage: ", localStorage.getItem("name"));
+// //remove data
+// localStorage.removeItem("name");
+// //clear all the data
+// localStorage.clear();
+
+//2. Session Storage -> Session storage is temporary storage mechanism that stores data only for the duration
+//                      of page session, once the browser tab is closed the data gets cleared.
+//                      Data persist only within the current tab or window.
+//                      Data is deleted when the tab is closed.
+//                      Maximum storage capacity is (5-10 MB).
