28231fe9dd600d70ab20b37e63fc3bef6d1e62e3
unknown <prachipanwar397@gmail.com>
2024-12-15 21:25:35 +0530

    wiggle sort optimal approach

--- DSA-102/wiggleSort.js
+++ DSA-102/wiggleSort.js
@@ -12,15 +12,25 @@
 // Explanation --->
 // nums[0] <= nums[1] >= nums[2] <= nums[3] >= nums[4] <= nums[5].
 
-//brute force approach
-function wiggleSort(arr, n){
-    //sort the entire array in acending order
-    arr.sort((a,b)=>a-b);
-    //loop through 1 to n-1 for odd indexes only
-    for(let i=1;i<n-1;i+=2){
-        [arr[i],arr[i+1]] = [arr[i+1],arr[i]];
-    }
-    return arr;
-}
+
 let arr = [1,5,1,1,6,4];
-console(wiggleSort(arr, 6));
+//optimal approach for wiggle sort TC - O(n)
+function wiggleSortOptimal(arr, n){
+   //loop through 1 to n the array
+   for(let i=1;i<n;i++){
+    //logic is element at odd index should be greater than its left and right index element
+    //check if odd index and element at odd index is less then the previous element 
+    if(i%2 === 1 && arr[i]<arr[i-1]){
+        //swap element at ith index to (i-1)th element
+        [arr[i],arr[i-1]] = [arr[i-1],arr[i]];
+    }
+    //for even index the element should be less than the next element(odd) so it it is greater then 
+    //we are swapping
+    if(i%2 === 0 && arr[i]>arr[i-1]){
+        //swap the elements
+        [arr[i],arr[i-1]] = [arr[i-1],arr[i]];
+    }
+   }
+   return arr;
+}
+console.log(wiggleSortOptimal(arr, 6));
