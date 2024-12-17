1e9e13c18bc738a9afb83ed2ba1e54a53e21a32d
unknown <prachipanwar397@gmail.com>
2024-12-17 19:53:11 +0530

    reverse vowels in a string

--- DSA-102/reverseVowels.js
+++ DSA-102/reverseVowels.js
@@ -1,3 +1,15 @@
+// Given a string s, reverse only all the vowels in the string and return it.
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases,
+// more than once.
+// Example 1:
+// Input: s = "IceCreAm"
+// Output: "AceCreIm"
+// Explanation:
+// The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+// Example 2:
+// Input: s = "leetcode"
+// Output: "leotcede"
+
 function swap(arr,a,b){
     let temp = arr[a];
     arr[a] = arr[b];
