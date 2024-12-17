69d23dbb4167b52db5a97d5bedd0727bd3893e87
unknown <prachipanwar397@gmail.com>
2024-12-17 17:24:56 +0530

    kid with maximum candy

---
+++ DSA-102/kidWithMaxCandy.js
@@ -0,0 +1,44 @@
+// There are n kids with candies. You are given an integer array candies, where each candies[i] 
+// represents the number of candies the ith kid has, and an integer extraCandies, denoting the 
+// number of extra candies that you have.
+// Return a boolean array result of length n, where result[i] is true if, after giving the ith kid 
+// all the extraCandies, they will have the greatest number of candies among all the kids, or 
+// false otherwise.
+// Note that multiple kids can have the greatest number of candies.
+
+// Example 1:
+// Input: candies = [2,3,5,1,3], extraCandies = 3
+// Output: [true,true,true,false,true] 
+// Explanation: If you give all extraCandies to:
+// - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+// - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+// - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+// - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+// - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+// Example 2:
+// Input: candies = [4,2,1,1,2], extraCandies = 1
+// Output: [true,false,false,false,false] 
+// Explanation: There is only 1 extra candy.
+// Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
+
+function kidWithCandies(candies, extraCandies){
+    let maxCandy = 0;
+    let result = [];
+    //iterate trhough the array and find the maximum candy
+    for(let i=0;i<candies.length;i++){
+        if(candies[i]>=maxCandy){
+            maxCandy = candies[i];
+        }
+    }
+    //iterate through the array and check if after giving extra candy to the kid is it the max
+    //or not
+    for(let i=0;i<candies.length;i++){
+        if(candies[i]+extraCandies >= maxCandy){
+            result.push(true);
+        }else{
+            result.push(false);
+        }
+    }
+    return result;
+}
+console.log(kidWithCandies([2,3,5,1,3],3));
