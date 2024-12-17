21735fb7ac201455be891b6d42493500a8612ff4
unknown <prachipanwar397@gmail.com>
2024-12-17 22:24:53 +0530

    compress the array of strings

---
+++ DSA-102/swapOddNdEvenBits.js
@@ -0,0 +1,16 @@
+// Problem Description
+// Given an unsigned integer N, swap all odd bits with even bits. Every even position bit is swapped 
+// with the adjacent bit on the right side and every odd position bit is swapped with adjacent on the
+// left side. Assume a 32 bit integer.
+// Hint: To avoid negative values caused by integer overflow, utilize BigInt in your JavaScript code 
+// for handling large integer computations.BigInt is a JavaScript built-in object used to represent 
+// integers larger than the range supported by the Number primitive.
+
+// Sample Input --->
+// 22
+
+// Sample Output --->
+// 41
+
+// Explanation --->
+// The given number is 22 (00010110), it should be converted to 41 (00101001).
