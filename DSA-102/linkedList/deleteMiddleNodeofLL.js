40433d4b8ad0e711ec879df0d0919fce732644e9
unknown <prachipanwar397@gmail.com>
2024-12-22 14:43:08 +0530

    odd even linked list

--- DSA-102/linkedList/deleteMiddleNodeofLL.js
+++ DSA-102/linkedList/deleteMiddleNodeofLL.js
@@ -62,3 +62,30 @@ function printLinkedList(head){
     return result;
 }
 console.log(printLinkedList(head));
+
+// Odd Even Linked List
+// Given the head of a singly linked list, group all the nodes with odd indices together followed by 
+// the nodes with even indices, and return the reordered list.
+// The first node is considered odd, and the second node is even, and so on.
+// Note that the relative order inside both the even and odd groups should remain as it was in the input.
+// You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+function oddEvenList(head){
+    if(!head || !head.next){
+        return head;
+    }
+    let odd = head;
+    let even = head.next;
+    let evenHead = even;
+    while(even && even.next){
+        odd.next = even.next;
+        odd = odd.next;
+        even.next = odd.next;
+        even = even.next;
+    }
+    odd.next = evenHead;
+    return head;
+}
+
+oddEvenList(head);
+console.log(printLinkedList(head));
