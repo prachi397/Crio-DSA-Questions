d4632363919ffb7e1b8e482c9613f17139811f86
unknown <prachipanwar397@gmail.com>
2024-12-22 14:17:15 +0530

    delete the middle node from the linked list

--- DSA-102/linkedList/creationofLL.js
+++ DSA-102/linkedList/creationofLL.js
@@ -2,29 +2,29 @@
 
 //start with a empty list
 let head = null;
-
-function addNode(value){
-    //created the node 
-    const newNode = {value,next:null};
-    if(!head){
-        // If list is empty, then new node itself will be head
-        head = newNode;
-    }else{
-        let current = head;
-        while(current.next){
-            current = current.next;
-        }
-        current.next = newNode;
-    }
-}
-function printList(){
+function addNode(value) {
+  //created the node
+  const newNode = { value, next: null };
+  if (!head) {
+    // If list is empty, then new node itself will be head
+    head = newNode;
+  } else {
     let current = head;
-    const values = [];
-    while(current){
-        values.push(current.value);
-        current = current.next;
+    while (current.next) {
+      current = current.next;
     }
-    return values.join("->");
+    current.next = newNode;
+  }
+}
+//printing linkedlist
+function printList() {
+  let current = head;
+  const values = [];
+  while (current) {
+    values.push(current.value);
+    current = current.next;
+  }
+  return values.join("->");
 }
 addNode(10);
 addNode(20);
---
+++ DSA-102/linkedList/deleteMiddleNodeofLL.js
@@ -0,0 +1,64 @@
+// You are given the head of a linked list. Delete the middle node, and return the head of the modified 
+// linked list.
+// The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, 
+// where ⌊x⌋ denotes the largest integer less than or equal to x.
+// For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+ 
+// Example 1:
+// Input: head = [1,3,4,7,1,2,6]
+// Output: [1,3,4,1,2,6]
+// Explanation:
+// The above figure represents the given linked list. The indices of the nodes are written below.
+// Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+// We return the new list after removing this node. 
+// Example 2:
+// Input: head = [1,2,3,4]
+// Output: [1,2,4]
+// Explanation:
+// The above figure represents the given linked list.
+// For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+// Example 3:
+// Input: head = [2,1]
+// Output: [2]
+// Explanation:
+// The above figure represents the given linked list.
+// For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+// Node 0 with value 2 is the only node remaining after removing node 1.
+
+class ListNode {
+    constructor(value = 0, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+const head = new ListNode(1);
+head.next = new ListNode(3);
+head.next.next = new ListNode(4);
+head.next.next.next = new ListNode(7);
+head.next.next.next.next = new ListNode(1);
+head.next.next.next.next.next = new ListNode(2);
+head.next.next.next.next.next.next = new ListNode(6);
+
+function deleteMiddle(head){
+    if(!head || !head.next){
+        return null;
+    }
+    let slow = head, fast = head, prev = null;
+    while(fast && fast.next){
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    prev.next = slow.next;
+    return head;
+}
+const mdofiedList = deleteMiddle(head);
+function printLinkedList(head){
+    let result = [];
+    while(head){
+        result.push(head.value);
+        head = head.next;
+    }
+    return result;
+}
+console.log(printLinkedList(head));
