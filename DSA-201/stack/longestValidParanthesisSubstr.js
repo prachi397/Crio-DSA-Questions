056b122021eabd1878c7b6cea5f202b132a488e6
unknown <prachipanwar397@gmail.com>
2024-12-24 11:51:29 +0530

    find longest valid paranthesis substring

---
+++ DSA-201/stack/longestValidParanthesisSubstr.js
@@ -0,0 +1,40 @@
+// Problem Description
+// Given a string S containing just the characters '(' and ')', find the length of the longest valid 
+// (well-formed) parentheses substring.
+
+// Sample Input 1
+// ()(()(())
+
+// Sample Output 1
+// 6
+
+// Explanation
+// The last 6 characters of the string forms the longest valid substring i.e. "()(())".
+
+function longestValidParentheses(s){
+    let stack = [];
+    let ans = 0;
+    stack.push(-1); //intially push -1 in the stack
+    //iterate over the string
+    for(let i=0;i<s.length;i++){
+        //if we found ( then push the index into stack
+        if(s[i] === '('){
+            stack.push(i);
+        }
+        //otherwise : )
+        else{
+            stack.pop();  //first pop the index from the stack
+            //check if stack became empty
+            if(stack.length === 0){
+                stack.push(i);  //then push the index
+            }
+            //stack has element
+            else{
+                let top = stack[stack.length-1]; //find the top index
+                ans = Math.max(ans,i-top);
+            }
+        }
+    }
+    return ans;
+}
+console.log(longestValidParentheses("()(()(())"));
