cbe0f1ab3eda0f19cee18e62dfcbf11fc4e44182
unknown <prachipanwar397@gmail.com>
2025-01-02 17:18:41 +0530

    promise chaining

--- javascript/promiseEg.js
+++ javascript/promiseEg.js
@@ -3,16 +3,41 @@ let isMomHappy = true;
 const willGetNewPhone = () => {
   return new Promise((resolve, reject) => {
     if (isMomHappy) {
-        let phone = {
-            brand: "samsung",
-            color: "black"
-        }
-      resolve(`Mom is happy, so you will get a brand new phone ${JSON.stringify(phone)}`);
+      let phone = {
+        brand: "samsung",
+        color: "black",
+      };
+      resolve(phone);
     } else {
       reject("Mom is angry, so you won't get anything");
     }
   });
 };
-willGetNewPhone()
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+// //consuming the promise
+// const askMom = function(){
+//     willGetNewPhone()
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+// }
+// askMom();
+
+//chaining of promises
+const showOff = function (phone) {
+  return new Promise((resolve, reject) => {
+    if (isMomHappy) {
+      resolve(`Hi friends, I have a new ${phone.color} ${phone.brand}`);
+    } else {
+      reject("I did not get phone.");
+    }
+  });
+};
+//consuming the chained promises
+const askMom = function () {
+  willGetNewPhone()
+    .then(showOff)
+    .then(function(fullfilled){
+        console.log(fullfilled);
+    })
+    .catch((err) => console.log(err));
+};
+askMom();
