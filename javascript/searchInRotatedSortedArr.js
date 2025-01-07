37d07696789c27e5f89418913b50533f554d1d7d
unknown <prachipanwar397@gmail.com>
2025-01-07 13:27:09 +0530

    search element in a rotated sorted array using binary search

--- javascript/searchInRotatedSortedArr.js
+++ javascript/searchInRotatedSortedArr.js
@@ -36,3 +36,56 @@ function search(nums, target){
     return -1;
 }
 console.log(search([4,5,6,7,0,1,2],6));
+
+//optimal approach using binary search
+
+//function to find pivot element, where the slop begans
+//here the loigc to find pivot is that the element which appears in first segment will be greater than or equal
+//to the first element and the element of second segment will always less than the first element
+function findPivot(nums){
+    let left=0, right = nums.length-1;
+    let ans = 0;
+    while(left<=right){
+        //get the mid
+        let mid = Math.floor((left+right)/2);
+        //check if mid lies in first segment
+        if(nums[mid]>=nums[0]){
+            ans = mid;  //update the answer to mid
+            left = mid+1;  //and search from the right side of mid
+        }
+        //if mid lies in second segment
+        else{
+            right = mid-1;
+        }
+    }
+    return ans;
+}
+//function to perform binary search
+function binarySearch(nums,left,right,target){
+    while(left<=right){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid] === target){
+            return mid;
+        }else if (nums[mid]>target){
+            right = mid-1;
+        }else{
+            left = mid+1;
+        }
+    }
+    return -1;
+}
+
+function searchOptimal(nums, target){
+   //find the pivot element
+   let pivotIdx = findPivot(nums);
+   //find the target in first segment
+   let leftAns = binarySearch(nums,0,pivotIdx,target);
+   if(leftAns !== -1){
+    return leftAns;
+   }
+   //find the target in second segment
+   return binarySearch(nums,pivotIdx+1,nums.length-1,target);
+}
+console.log(searchOptimal([4,5,6,7,0,1,2],7));
+
+//here tc is O(logn) and sc is O(1)
